3cd1d7b937b3ad874f8e52d52acb97e79a8af132
Abieiysusweas1 <abelrebollo24@gmail.com>
2025-06-16 22:06:16 +0200

    Cambios en el PHP de introducción 5

--- php/introduccion-dat.php
+++ php/introduccion-dat.php
@@ -20,14 +20,12 @@
    $c_ale = isset($_POST["c_ale"]) ? 1 : 0;
    $c_rum = isset($_POST["c_rum"]) ? 1 : 0;
 
-   $conexion = mysqli_connect($host,$usuario,$contrasena,$base_datos) or die("Hubo un error en la conexión.");
-
-   $sql = "INSERT INTO usuarios(usuario,contra,idioma,c_ing,c_esp,c_fra,c_ita,c_ale,c_rum) VALUES ('$_POST[$user]',
-   '$_POST[$contra]','$_POST[$lan]','$_POST[$c_ing]','$_POST[$c_esp]','$_POST[$c_fra]','$_POST[$c_ita]','$_POST[$c_ale]','$_POST[$c_rum]')";
+   $sql = "INSERT INTO usuarios(usuario,contra,idioma,c_ing,c_esp,c_fra,c_ita,c_ale,c_rum) 
+   VALUES ($user,$contra,$lan,$c_ing,$c_esp,$c_fra,$c_ita,$c_ale,$c_rum)";
    
    mysqli_query($conexion,$sql) or die("Ocurrió un error en la consulta".mysqli_error($conexion));
 
-   mysql_close($conexion);
+   mysqli_close($conexion);
 ?>
 <!DOCTYPE html>
 <html lang="es">
